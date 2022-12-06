05ea8cf063955ed494fd0bd5f7c918e5f292622b
Saurabh <Saurabh@DESKTOP-159FJR9>
2022-12-06 23:45:51 +0530

    First Commit

---
+++ programs/src/programs/prog3.java
@@ -0,0 +1,14 @@
+package programs;
+
+public class prog3 {
+
+	public static void main(String[] args) {
+		 
+		int pen = 500;
+		float f = 10.15f;
+		double percentage = pen * f;
+		System.out.println(percentage);
+
+	}
+
+}
