73b6701eb7975dee5b1ac187abc7351368859080
Saurabh <Saurabh@DESKTOP-159FJR9>
2022-12-06 23:40:13 +0530

    First Commit

---
+++ programs/src/programs/prog2.java
@@ -0,0 +1,23 @@
+package programs;
+
+public class prog2 {
+
+	public static void main(String[] args) {
+		
+		int max=2147483647;
+		int min=-2147483647;
+		
+		short shortMax=32767;
+		short shortMin=-32767;
+		
+		long longMax=922337203854775807L;
+		long longMin=-92233720385477580L;
+		
+		byte byteMax=127;
+		byte byteMin=-127;
+		System.out.println(longMax);
+		
+
+	}
+
+}
