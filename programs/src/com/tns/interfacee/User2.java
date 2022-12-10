280803cc6edfc505752a487e04228ddb7efc0790
Saurabh <Saurabh@DESKTOP-159FJR9>
2022-12-10 13:30:58 +0530

    First Commit

---
+++ programs/src/com/tns/interfacee/User2.java
@@ -0,0 +1,20 @@
+package com.tns.interfacee;
+
+public class User2 {
+
+
+	public static void main(String[] args) {
+		int a[][] = { { 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } };
+		for (int i = 0; i < a.length; i++) 
+		{
+			for (int j = 0; j < a.length; j++) 
+			{
+				System.out.print(a[i][j] + " ");
+
+			}
+		}
+		System.out.println();
+
+	}
+
+}
---
+++ programs/src/programs/Prog11.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Prog11 {
+
+	public static void main(String[] args) {
+		int a=10;  
+		int b=5;  
+		int min=(a<b)?a:b;  
+		System.out.println(min); 
+
+	}
+
+}
