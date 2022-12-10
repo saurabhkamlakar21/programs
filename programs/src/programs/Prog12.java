fd6c01c0f8bc386638b9fe316bf82bd67f561cb9
Saurabh <Saurabh@DESKTOP-159FJR9>
2022-12-10 13:34:03 +0530

    First Commit

---
+++ programs/src/programs/Prog12.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class Prog12 {
+
+	public static void main(String[] args) {
+		int a=10;  
+		int b=20;  
+		a+=4;  
+		b-=4;
+		System.out.println(a);  
+		System.out.println(b); 
+
+	}
+
+}
