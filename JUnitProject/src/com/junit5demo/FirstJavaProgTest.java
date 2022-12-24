3dd7fa05a9ce800b50fc657614ea4fb09481726a
Saurabh <Saurabh@DESKTOP-159FJR9>
2022-12-24 13:10:27 +0530

    First Commit

---
+++ JUnitProject/src/com/junit5demo/FirstJavaProgTest.java
@@ -0,0 +1,20 @@
+package com.junit5demo;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstJavaProgTest {
+
+		
+		@Test
+		void disp()
+		{
+			System.out.println("Hello C2TC students");
+		}
+
+
+         @Test
+         void disp2()
+         {
+        	 System.out.println("Hello TechnoServe");
+         }
+}
